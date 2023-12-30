0f525a3c1e00e07bea157af524e4e71e45dd72c6
Paulo Giovani <pg_faria@yahoo.com.br>
2023-12-30 19:21:39 -0300

    Project Page Corrections

--- src/scenes/Projects.jsx
+++ src/scenes/Projects.jsx
@@ -4,9 +4,9 @@ import { motion } from "framer-motion";
 const container = {
     hidden: {},
     visible: {
-      transition: {
-        staggerChildren: 0.2,
-      },
+        transition: {
+            staggerChildren: 0.2,
+        },
     },
 };
   
@@ -62,7 +62,7 @@ const Projects = () => {
                         <span className="text-red">PRO</span>JECTS
                     </p>
                     <div className="flex justify-center mt-5">
-                        <LineGradient width="w-1/3" />
+                        <LineGradient width="w-2/3" />
                     </div>
                 </div>
                 <p className="mt-10 mb-10">
@@ -76,10 +76,10 @@ const Projects = () => {
             <div className="flex justify-center">
                 <motion.div
                     className="sm:grid sm:grid-cols-3"
+                    variants={container}
                     initial="hidden"
                     whileInView="visible"
-                    viewport={{ once: true, amount: 0.5 }}
-                    variants={container}
+                    viewport={{ once: true, amount: 0.2 }}
                 >
 
                     {/* Linha 1 */}
@@ -89,6 +89,7 @@ const Projects = () => {
                     >
                         BEAUTIFUL USER INTERFACES
                     </div>
+
                     <Project title="Project 1" />
                     <Project title="Project 2" />
             
@@ -100,6 +101,7 @@ const Projects = () => {
                     {/* ROW 3 */}
                     <Project title="Project 6" />
                     <Project title="Project 7" />
+
                     <div
                         className="flex justify-center text-center items-center p-10 bg-blue
                         max-w-[400px] max-h-[400px] text-2xl font-playfair font-semibold"
